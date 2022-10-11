f622cb33a9c97fdb1b28e1ed2d22da9df09fdc44
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-10-11 02:24:57 +0100

    more tasks

--- 05/4-list_division.py
+++ 05/4-list_division.py
@@ -13,8 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
-        except BaseException:
-            result.append(0)
         else:
             result.append(q)
     return result
