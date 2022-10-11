abc8c2252dfc08f1d097cbd268d44db4be76124e
Henry Ozomgbachi <ozomgbachih@gmail.com>
2022-10-11 01:37:24 +0100

    more tasks

--- 05/0-safe_print_list.py
+++ 05/0-safe_print_list.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """prints x elements of a list
-
-    Args:
-        my_list (list, optional): Input List. Defaults to [].
-        x (int, optional): num of el to print. Defaults to 0.
-
-    Returns:
-        int: real number of elements printed
-    """
     num = 0
     try:
         for i in range(0,x):
---
+++ 05/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(0, list_length):
+        try:
+            q = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        except BaseException:
+            result.append(0)
+        else:
+            result.append(q)
+    return result
